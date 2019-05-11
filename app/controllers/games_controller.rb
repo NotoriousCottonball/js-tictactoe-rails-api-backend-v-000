fe27ea19f64269a2352ea11ed220668779875da0
George  Pianka <gpianka1@gmail.com>
2019-05-11 07:52:56 +0000

    Automatically backed up by Learn

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,28 +4,28 @@ class GamesController < ApplicationController
    def index
     games = Game.all
     render json: games
-  end
+   end
 
    def update
     @game.update(game_params)
     render json: @game
-  end
+   end
 
    def create
     game = Game.create(game_params)
     render json: game, status: 201
-  end
+   end
 
    def show
     render json: @game
-  end
+   end
 
    private
 
    def game_params
     params.permit(state: [])
-  end
+   end
 
    def set_game
     @game = Game.find(params[:id])
-  end
+   end
